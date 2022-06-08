f8730b2d3e4a35ae1e78534aad9377499f5c302a
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-08 18:23:54 -0400

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,64 @@
+/*
+(c) 2022 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+let filesystemAPI = ('showOpenFilePicker' in window && 'showDirectoryPicker' in window);
+let cryptoAPI = ('crypto' in self);
+
+let width;
+let height;
+const arrRootEntries = [];
+const mapFileKeys = new Map();
+
+const promiseLoad = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+promiseLoad.then(function () {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  document.body.innerHTML = "Loaded";
+});
+
+
+// Encrypts the given plaintext with the given key using the given iv, if provided
+// Using AES-256 block cipher in CBC mode
+// Padded with PKCS#7, (RFC2315 Section 10.3, step 2)
+// Uint8Array is used, instead of ArrayBuffer, to allow a portion of a buffer to be used
+// plaintext: (Uint8Array) plain (unencrypted) contents
+// key: (Uint8Array) key to use to encrypt contents
+// iv: (Uint8Array, optional) initialization vector
+// Returns : (Promise, resolving to Object, containing members iv and ciphertext)
+//   iv: (Uint8Array) initialization vector
+//   ciphertext: (ArrayBuffer) encrypted contents
+function encrypt_AES256_CBC(plaintext, key, iv) {
+  let retVal = {};
+  if (!iv) {
+    retVal.iv = new Uint8Array(randomData(16));
+  }
+  return window.crypto.subtle.importKey("raw", key, "AES-CBC", false, [ "encrypt", "decrypt" ]).then(function (myImportedKey) {
+    let myAesCbcParams = {name: "AES-CBC", iv: retVal.iv};
+    return window.crypto.subtle.encrypt(myAesCbcParams, myImportedKey, plaintext);
+  }).then(function (ciphertext) {
+    retVal.ciphertext = ciphertext;
+    return retVal;
+  });
+}
+
+// Decrypts the given ciphertext with the given key
+// Using AES-256 block cipher in CBC mode
+// Assumes padding of PKCS#7, (RFC2315 Section 10.3, step 2)
+// Uint8Array is used, instead of ArrayBuffer, to allow a portion of a buffer to be used
+// ciphertext: (Uint8Array) encrypted contents
+// key: (Uint8Array) key to decrypt the contents
+// iv: (Uint8Array) initialization vector
+// Returns : (Promise, resolving to ArrayBuffer) plain (unencrypted) contents
+function decrypt_AES256_CBC(ciphertext, key, iv) {
+  return window.crypto.subtle.importKey("raw", key, "AES-CBC", false, [ "encrypt", "decrypt" ]).then(function (myImportedKey) {
+    let myAesCbcParams = {name: "AES-CBC", iv: iv};
+    return window.crypto.subtle.decrypt(myAesCbcParams, myImportedKey, ciphertext);
+  });
+}
