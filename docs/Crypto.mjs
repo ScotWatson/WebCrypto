a3985953e5265db232b3de2ef1a92e132959e249
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-09 21:12:28 -0400

    Update Crypto.mjs

--- docs/Crypto.mjs
+++ docs/Crypto.mjs
@@ -14,13 +14,13 @@ export class Crypto {
 // typedArray: (TypedArray, but not Float32Array nor Float64Array) All elements in the array will be overwritten with random numbers
 // Return: the typedArray (not a copy)
 // QuotaExceededError - Thrown if the byteLength of typedArray exceeds 65,536.
-function getRandomValues(typedArray) {
+export function getRandomValues(typedArray) {
   return self.crypto.getRandomValues(typedArray);
 }
 
 // Returns a randomly generated, 36 character long v4 UUID.
 // Return: (String) containing a randomly generated, 36 character long v4 UUID
-function randomUUID() {
+export function randomUUID() {
   return self.crypto.randomUUID();
 }
 
@@ -32,7 +32,7 @@ function randomUUID() {
 //   Unless your application calls for a label, you can just omit this argument and it will not affect the security of the encryption operation.
 // To use RSA-OAEP, pass an RsaOaepParams object.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
-function decrypt_RSA_OAEP(key, data, label) {
+export function decrypt_RSA_OAEP(key, data, label) {
   const algorithm = {
     name: "RSA-OAEP",
     label: label,
@@ -47,7 +47,7 @@ function decrypt_RSA_OAEP(key, data, label) {
 // length: (Number) the number of bits in the counter block that are used for the actual counter. The counter must be big enough that it doesn't wrap: if the message is n blocks and the counter is m bits long, then the following must be true: n <= 2^m. The NIST SP800-38A standard, which defines CTR, suggests that the counter should occupy half of the counter block (see Appendix B.2), so for AES it would be 64.
 // To use AES-CTR, pass an AesCtrParams object.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
-function decrypt_AES_CTR(key, data, counter, length) {
+export function decrypt_AES_CTR(key, data, counter, length) {
   const algorithm = {
     name: "AES-CTR",
     counter: counter,
@@ -62,7 +62,7 @@ function decrypt_AES_CTR(key, data, counter, length) {
 // iv: (BufferSource) The initialization vector. Must be 16 bytes, unpredictable, and preferably cryptographically random. However, it need not be secret (for example, it may be transmitted unencrypted along with the ciphertext)
 // To use AES-CBC, pass an AesCbcParams object.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
-function decrypt_AES_CBC(key, data, iv) {
+export function decrypt_AES_CBC(key, data, iv) {
   const algorithm = {
     name: "AES-CBC",
     iv: iv,
@@ -81,7 +81,7 @@ function decrypt_AES_CBC(key, data, iv) {
 //     According to the Web Crypto specification this must have one of the following values: 32, 64, 96, 104, 112, 120, or 128. The AES-GCM specification recommends that it should be 96, 104, 112, 120 or 128, although 32 or 64 bits may be acceptable in some applications: Appendix C of the specification provides additional guidance here., defaults to 128 if it is not specified.
 // To use AES-GCM, pass an AesGcmParams object.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
-function decrypt_AES_GCM(key, data, iv, additionalData, tagLength) {
+export function decrypt_AES_GCM(key, data, iv, additionalData, tagLength) {
   const algorithm = {
     name: "AES-GCM",
     iv: iv,
@@ -96,7 +96,7 @@ function decrypt_AES_GCM(key, data, iv, additionalData, tagLength) {
 // length: (Number) representing the number of bits to derive. To be compatible with all browsers, the number should be a multiple of 8.
 // public: (CryptoKey) the public key of the other entity
 // Return: (Promise that fulfills with an ArrayBuffer) containing the derived bits
-function deriveBits_ECDH(algorithm, baseKey, length, public) {
+export function deriveBits_ECDH(algorithm, baseKey, length, public) {
   const algorithm = {
     name: "ECDH",
     public: public,
@@ -107,18 +107,61 @@ function deriveBits_ECDH(algorithm, baseKey, length, public) {
 // derive an array of bits from a base key
 // baseKey: (CryptoKey) representing the input to the derivation algorithm. If algorithm is ECDH, this will be the ECDH private key. Otherwise it will be the initial key material for the derivation function: for example, for PBKDF2 it might be a password, imported as a CryptoKey using SubtleCrypto.importKey().
 // length: (Number) representing the number of bits to derive. To be compatible with all browsers, the number should be a multiple of 8.
-// hash: (String) representing the digest algorithm to use. This may be one of:
-//        SHA-1
-//        SHA-256
-//        SHA-384
-//        SHA-512
 // salt: (BufferSource) The HKDF specification states that adding salt "adds significantly to the strength of HKDF". Ideally, the salt is a random or pseudo-random value with the same length as the output of the digest function. Unlike the input key material passed into deriveKey(), salt does not need to be kept secret.
 // info: (BufferSource) representing application-specific contextual information. This is used to bind the derived key to an application or context, and enables you to derive different keys for different contexts while using the same input key material. It's important that this should be independent of the input key material itself. This property is required but may be an empty buffer.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the derived bits
-function deriveBits_HKDF(algorithm, baseKey, length) {
+export function deriveBits_HKDF_SHA1(algorithm, baseKey, length, salt, info) {
+  const algorithm = {
+    name: "HKDF",
+    hash: "SHA-1",
+    salt: salt,
+    info: info,
+  };
+  return self.crypto.subtle.deriveBits(algorithm, baseKey, length);
+}
+
+// derive an array of bits from a base key
+// baseKey: (CryptoKey) representing the input to the derivation algorithm. If algorithm is ECDH, this will be the ECDH private key. Otherwise it will be the initial key material for the derivation function: for example, for PBKDF2 it might be a password, imported as a CryptoKey using SubtleCrypto.importKey().
+// length: (Number) representing the number of bits to derive. To be compatible with all browsers, the number should be a multiple of 8.
+// salt: (BufferSource) The HKDF specification states that adding salt "adds significantly to the strength of HKDF". Ideally, the salt is a random or pseudo-random value with the same length as the output of the digest function. Unlike the input key material passed into deriveKey(), salt does not need to be kept secret.
+// info: (BufferSource) representing application-specific contextual information. This is used to bind the derived key to an application or context, and enables you to derive different keys for different contexts while using the same input key material. It's important that this should be independent of the input key material itself. This property is required but may be an empty buffer.
+// Return: (Promise that fulfills with an ArrayBuffer) containing the derived bits
+export function deriveBits_HKDF_SHA256(algorithm, baseKey, length, salt, info) {
+  const algorithm = {
+    name: "HKDF",
+    hash: "SHA-256",
+    salt: salt,
+    info: info,
+  };
+  return self.crypto.subtle.deriveBits(algorithm, baseKey, length);
+}
+
+// derive an array of bits from a base key
+// baseKey: (CryptoKey) representing the input to the derivation algorithm. If algorithm is ECDH, this will be the ECDH private key. Otherwise it will be the initial key material for the derivation function: for example, for PBKDF2 it might be a password, imported as a CryptoKey using SubtleCrypto.importKey().
+// length: (Number) representing the number of bits to derive. To be compatible with all browsers, the number should be a multiple of 8.
+// salt: (BufferSource) The HKDF specification states that adding salt "adds significantly to the strength of HKDF". Ideally, the salt is a random or pseudo-random value with the same length as the output of the digest function. Unlike the input key material passed into deriveKey(), salt does not need to be kept secret.
+// info: (BufferSource) representing application-specific contextual information. This is used to bind the derived key to an application or context, and enables you to derive different keys for different contexts while using the same input key material. It's important that this should be independent of the input key material itself. This property is required but may be an empty buffer.
+// Return: (Promise that fulfills with an ArrayBuffer) containing the derived bits
+export function deriveBits_HKDF(algorithm, baseKey, length, salt, info) {
   const algorithm = {  
     name: "HKDF",
-    hash: hash,
+    hash: "SHA-384",
+    salt: salt,
+    info: info,
+  };
+  return self.crypto.subtle.deriveBits(algorithm, baseKey, length);
+}
+
+// derive an array of bits from a base key
+// baseKey: (CryptoKey) representing the input to the derivation algorithm. If algorithm is ECDH, this will be the ECDH private key. Otherwise it will be the initial key material for the derivation function: for example, for PBKDF2 it might be a password, imported as a CryptoKey using SubtleCrypto.importKey().
+// length: (Number) representing the number of bits to derive. To be compatible with all browsers, the number should be a multiple of 8.
+// salt: (BufferSource) The HKDF specification states that adding salt "adds significantly to the strength of HKDF". Ideally, the salt is a random or pseudo-random value with the same length as the output of the digest function. Unlike the input key material passed into deriveKey(), salt does not need to be kept secret.
+// info: (BufferSource) representing application-specific contextual information. This is used to bind the derived key to an application or context, and enables you to derive different keys for different contexts while using the same input key material. It's important that this should be independent of the input key material itself. This property is required but may be an empty buffer.
+// Return: (Promise that fulfills with an ArrayBuffer) containing the derived bits
+export function deriveBits_HKDF(algorithm, baseKey, length, salt, info) {
+  const algorithm = {  
+    name: "HKDF",
+    hash: "SHA-512",
     salt: salt,
     info: info,
   };
