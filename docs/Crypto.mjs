f5785e8de4590add0b0d5554cab526b821b744a0
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-11 15:30:13 -0400

    Update Crypto.mjs

--- docs/Crypto.mjs
+++ docs/Crypto.mjs
@@ -3589,7 +3589,7 @@ export function deriveKey_PBKDF2_SHA512_AES256_KW(baseKey, salt, iterations) {
 // Don't use this in cryptographic applications
 // data: (ArrayBuffer or ArrayBufferView) containing the data to be digested.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the digest.
-export function digest_SHA_1(data) {
+export function digest_SHA1(data) {
   const algorithm = "SHA-1";
   return self.crypto.subtle.digest(algorithm, data);
 }
@@ -3597,7 +3597,7 @@ export function digest_SHA_1(data) {
 // hashes the message
 // data: (ArrayBuffer or ArrayBufferView) containing the data to be digested.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the digest.
-export function digest_SHA_256(data) {
+export function digest_SHA256(data) {
   const algorithm = "SHA-256";
   return self.crypto.subtle.digest(algorithm, data);
 }
@@ -3605,7 +3605,7 @@ export function digest_SHA_256(data) {
 // hashes the message
 // data: (ArrayBuffer or ArrayBufferView) containing the data to be digested.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the digest.
-export function digest_SHA_384(data) {
+export function digest_SHA384(data) {
   const algorithm = "SHA-384";
   return self.crypto.subtle.digest(algorithm, data);
 }
@@ -3613,12 +3613,72 @@ export function digest_SHA_384(data) {
 // hashes the message
 // data: (ArrayBuffer or ArrayBufferView) containing the data to be digested.
 // Return: (Promise that fulfills with an ArrayBuffer) containing the digest.
-export function digest_SHA_512(data) {
+export function digest_SHA512(data) {
   const algorithm = "SHA-512";
   return self.crypto.subtle.digest(algorithm, data);
 }
 
-export function encrypt() {
+// Encrypts some encrypted data using RSA-OAEP
+// key: (CryptoKey)
+// data: (BufferSource)
+// label: (BufferSource) An array of bytes that does not itself need to be encrypted but which should be bound to the ciphertext
+//   A digest of the label is part of the input to the encryption operation.
+//   Unless your application calls for a label, you can just omit this argument and it will not affect the security of the encryption operation.
+// Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
+export function encrypt_RSA_OAEP(key, data, label) {
+  const algorithm = {
+    name: "RSA-OAEP",
+    label: label,
+  };
+  return self.crypto.subtle.decrypt(algorithm, key, data);
+}
+
+// Encrypts some encrypted data using AES-CTR
+// key: (CryptoKey)
+// data: (BufferSource)
+// counter: (BufferSource) the initial value of the counter block. This must be 16 bytes long (the AES block size). The rightmost length bits of this block are used for the counter, and the rest is used for the nonce. For example, if length is set to 64, then the first half of counter is the nonce and the second half is used for the counter.,
+// length: (Number) the number of bits in the counter block that are used for the actual counter. The counter must be big enough that it doesn't wrap: if the message is n blocks and the counter is m bits long, then the following must be true: n <= 2^m. The NIST SP800-38A standard, which defines CTR, suggests that the counter should occupy half of the counter block (see Appendix B.2), so for AES it would be 64.
+// Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
+export function encrypt_AES_CTR(key, data, counter, length) {
+  const algorithm = {
+    name: "AES-CTR",
+    counter: counter,
+    length: length,
+  };
+  return self.crypto.subtle.decrypt(algorithm, key, data);
+}
+
+// Encrypts some encrypted data using AES-CBC
+// key: (CryptoKey)
+// data: (BufferSource)
+// iv: (BufferSource) The initialization vector. Must be 16 bytes, unpredictable, and preferably cryptographically random. However, it need not be secret (for example, it may be transmitted unencrypted along with the ciphertext)
+// Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
+export function decrypt_AES_CBC(key, data, iv) {
+  const algorithm = {
+    name: "AES-CBC",
+    iv: iv,
+  };
+  return self.crypto.subtle.encrypt(algorithm, key, data);
+}
+
+// Encrypts some encrypted data using AES-GCM
+// key: (CryptoKey)
+// data: (BufferSource)
+// iv: (BufferSource) the initialization vector. This must be unique for every encryption operation carried out with a given key. Put another way: never reuse an IV with the same key. The AES-GCM specification recommends that the IV should be 96 bits long, and typically contains bits from a random number generator. Section 8.2 of the specification outlines methods for constructing IVs. Note that the IV does not have to be secret, just unique: so it is OK, for example, to transmit it in the clear alongside the encrypted message.
+// additionalData: (Optional) (BufferSource). This contains additional data that will not be encrypted but will be authenticated along with the encrypted data. If additionalData is given here then the same data must be given in the corresponding call to decrypt(): if the data given to the decrypt() call does not match the original data, the decryption will throw an exception. This gives you a way to authenticate associated data without having to encrypt it.
+//     The bit length of additionalData must be smaller than 2^64 - 1.
+//     The additionalData property is optional and may be omitted without compromising the security of the encryption operation.
+// tagLength: (Optional) (Number) This determines the size in bits of the authentication tag generated in the encryption operation and used for authentication in the corresponding decryption.
+//     According to the Web Crypto specification this must have one of the following values: 32, 64, 96, 104, 112, 120, or 128. The AES-GCM specification recommends that it should be 96, 104, 112, 120 or 128, although 32 or 64 bits may be acceptable in some applications: Appendix C of the specification provides additional guidance here., defaults to 128 if it is not specified.
+// Return: (Promise that fulfills with an ArrayBuffer) containing the plaintext.
+export function decrypt_AES_GCM(key, data, iv, additionalData, tagLength) {
+  const algorithm = {
+    name: "AES-GCM",
+    iv: iv,
+    additionalData: additionalData,
+    tagLength: tagLength,
+  };
+  return self.crypto.subtle.encrypt(algorithm, key, data);
 }
 
 export function exportKey() {
